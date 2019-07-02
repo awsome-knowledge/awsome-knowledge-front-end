32c8620382833ddb83ef8ff3a338ef1036269aa2
qiufeihong2018 <15058301288@163.com>
2019-07-02 21:54:31 +0800

    add algorithm

---
+++ coding/twosum.js
@@ -0,0 +1,16 @@
+/**
+ * @param {number[]} nums
+ * @param {number} target
+ * @return {number[]}
+ */
+var twoSum = function (nums, target) {
+    let map = {}
+    for (let i = 0; i < nums.length; i++) {
+        let tep = target - nums[i]
+        let val = map.hasOwnProperty(tep)
+        if (val) {
+            return [map[tep], i]
+        }
+        map[nums[i]] = i
+    }
+};
